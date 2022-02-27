1129fba4a3ed6cda7480b99137ca9787a8bd7984
lanbairua <89310348+lanbairua@users.noreply.github.com>
2022-02-28 00:59:25 +0800

    栈

--- stack.py
+++ stack.py
@@ -4,6 +4,7 @@ from stack_base import *
 
 class Stack(StackBase):
     def __getattribute__(self, item):
+        # type: (str) -> object
         """ 实例化后is_insurance为真, 可以直接访问peek """
         return super(StackBase if item in ["peek"] else Stack, self).__getattribute__(item)
         # 暂时没有想到什么魔法方法替代peek
